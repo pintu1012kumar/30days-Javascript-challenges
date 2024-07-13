a4bdff40f5b3f77a0d58a68d8d26f4d12c11f767
Pintu kumar <pintu1012kumar@gmail.com>
2024-07-13 10:32:54 +0530

    day1

---
+++ Day1.js
@@ -0,0 +1,33 @@
+ // Activity 1
+var x=1;
+let y=3
+console.log(x,y);
+
+// Activity 2
+const z= false;
+console.log(z);
+
+// Activity 3
+let a=10;
+var b="string"
+const c=true;
+let d = {
+    name:"pintu",
+    age: 20,
+    rollno:100100,
+}
+const arr =[1,1,3,4,2];
+console.log(
+typeof(a),
+typeof(b),
+typeof(c),
+typeof(d),
+typeof(arr)
+)
+
+// Activity 4
+let first=10;
+first =20;
+console.log(first);
+
+
