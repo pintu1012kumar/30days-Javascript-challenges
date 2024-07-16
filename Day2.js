97c872519704aefdf5df9f2a0ec327a22595107f
Pintu kumar <pintu1012kumar@gmail.com>
2024-07-16 08:56:28 +0530

    Day2

---
+++ Day2.js
@@ -0,0 +1,29 @@
+// Operators
+
+// Activity-1
+
+let x= 20;
+let y= 4;
+
+
+console.log("Add=",x+y);
+console.log("Sub=",x-y);
+console.log("Mul=",x*y);
+console.log("Div=",x/y);
+console.log("Add=",x%y);
+
+ x+=10;
+ y-=1;
+console.log("+=",x);
+console.log("-=",y);
+
+let a=10;
+let b="10";
+
+console.log(a==b);
+console.log(a===b);
+
+let rs=x>0?"positive":"negative";
+console.log(rs);
+
+
