2cc8508f79e74677a6bc66a77a24505bc65aebfb
Pintu kumar <pintu1012kumar@gmail.com>
2024-07-19 08:53:29 +0530

    Day4

---
+++ Day4.js
@@ -0,0 +1,39 @@
+// Task-1
+
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+// Task-2
+
+for(let i=1;i<=10;i++){
+    console.log(i*5);
+}
+
+// Task-3
+
+let sum =0;
+let j=1;
+while(j<=10){
+    sum +=j;
+    j++;
+}
+console.log("sum=",sum);
+
+// Task-4
+
+
+let k=10;
+while(k>0){
+    console.log("reverse=",k);
+    k--;
+}
+
+
+// Task -7 try to solving
+
+// for(let i=0;i<5;i++){
+//     for(let j=0;j<i+1;j++){
+//         console.log("*");
+//     }
+// }
