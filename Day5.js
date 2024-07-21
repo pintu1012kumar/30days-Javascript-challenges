0ae07352ebbbd28a297d66eef434c2cb1287f344
Pintu kumar <pintu1012kumar@gmail.com>
2024-07-21 11:42:59 +0530

    Day5

---
+++ Day5.js
@@ -0,0 +1,36 @@
+// Task 1
+
+function checkNum(num)  {
+    if(num%2==0){
+        console.log("num is even");
+    }
+    else console.log("num is odd");
+}
+
+let x= checkNum(11);
+
+
+// Task 2
+function findsqr(num)  {
+   return num*num;
+}
+
+let y= findsqr(11);
+console.log("Sqr of num=",y);
+
+// Task 4
+function conct(a,b)  {
+    return a+b;
+ }
+ 
+ let z= conct("pintu","kumar");
+ console.log("conct of str=",z);
+
+ // Task 5
+let p = (f,s)=> {
+    return f+s;
+}
+
+console.log(p(4,5));
+
+
